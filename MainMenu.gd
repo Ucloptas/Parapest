ac31073273901a6d87d996e779f4c35d7aea29c0
Papaye98 <sinancakir4203@gmail.com>
2025-11-08 14:09:08 -0600

    Parent Login Page Integrated
    Parent Login Page integrated to Godot as a scene. As of this commit there is no back end integration only UI.

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,19 +1,10 @@
 extends Control
 
-# Create the dialog once when the scene loads
-var dialog: AcceptDialog
-
-func _ready() -> void:
-	dialog = AcceptDialog.new()
-	dialog.title = "Parent Login"
-	dialog.dialog_text = "Parent portal coming soon."
-	add_child(dialog)
-
 func _on_parent_login_button_down() -> void:
-	dialog.popup_centered()
+	get_tree().change_scene_to_file("res://parent_login.tscn")
 
 func _on_chore_xplorer_button_down() -> void:
-	get_tree().change_scene_to_file("res://chore_xplorer.tscn")  # adjust path if needed
+	get_tree().change_scene_to_file("res://chore_xplorer.tscn")
 
 func _on_exit_button_down() -> void:
 	get_tree().quit()

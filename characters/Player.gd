042c2ed881223ee218fbe62bd6bc783646745b3a
Ucloptas <96969145+Ucloptas@users.noreply.github.com>
2025-11-27 10:05:54 -0600

    Finalized pink player animations and logic

--- characters/Player.gd
+++ characters/Player.gd
@@ -2,14 +2,15 @@ extends CharacterBody2D
 
 const SPEED = 400.0
 const JUMP_FORCE = -500.0
-const GRAVITY = 700.0
-@onready var animationSprites=$AnimationSprites
-@onready var anim=$Animations
+const GRAVITY = 300.0
+@onready var anim:AnimationPlayer=$Animations
+@onready var sprites = $AnimationSprites
 var current_anim
 
 var is_jumping = false
 var is_falling = false
 var is_sprinting = false
+var can_move = true
 
 func _ready() -> void:
 	add_to_group("player")
@@ -20,46 +21,62 @@ func _physics_process(delta: float) -> void:
 		velocity.y += GRAVITY * delta
 
 	#get horizontal input direction: -1, 0, 1 // left, neutral, right
+	
 	var input_dir_h = Input.get_axis("ui_left", "ui_right")
-	velocity.x = input_dir_h * SPEED
+	if can_move:
+		velocity.x = input_dir_h * SPEED
 
 	#sprite horizontal flip
-	if input_dir_h > 0:
-		animationSprites.flip_h=false
-	elif input_dir_h < 0:
-		animationSprites.flip_h=true
+	if input_dir_h > 0 && can_move:
+		sprites.flip_h=false
+	elif input_dir_h < 0 && can_move:
+		sprites.flip_h=true
 	
 	#animation triggers
 	
 	if is_on_floor():
-		if is_falling: #detect landing
+		if Input.is_action_just_pressed("attack"):
+			anim.play("attack_animation")
+			can_move=false
+			return
+		elif !can_move: #if movement locked, dont do any of this until its allowed again
+			return
+		elif is_falling: #detect landing
 			is_falling = false
 			play("land_animation")
 			return
-		elif Input.is_action_just_pressed("move_up"): #execute jump
+		elif Input.is_action_just_pressed("move_up") && !is_jumping: #execute jump
 			play("jump_animation")
 			is_jumping = true
-			is_falling = false
 		elif is_jumping: #if jumping, dont interrupt
 			pass
-		elif(input_dir_h!=0): #detect idling
-			play("walking_animation")
+		elif input_dir_h!=0: #detect idling
+			play("walk_animation")
 		else:
 			play("idle_animation")
 	else:
-		if velocity.y > 0 && is_jumping == true: #if player just started falling
+		if velocity.y > 0: #if player just started falling
 			is_jumping = false
 			is_falling = true
 			play("fall_animation")
-
+	
 	# Move
 	move_and_slide()
 	
 func play(anim_name: String): #function to manage animation calls and currently playing animation
-	if name != current_anim:
+	#if no input, reset animation
+	if anim_name == "": 
+		anim.play("idle_animation")
+		current_anim = "idle_animation"
+		can_move=true
+	if anim_name != current_anim:
+		if !can_move:
+			can_move=true
 		anim.play(anim_name)
 		current_anim = anim_name
 		
 func do_jump(): #called from jump animation at right time
 	velocity.y = JUMP_FORCE
+	
+
 	

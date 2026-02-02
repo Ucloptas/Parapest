c976ae50dc57bba09320c5c94e1f2a53d68135c9
Ucloptas <96969145+Ucloptas@users.noreply.github.com>
2026-02-01 22:13:36 -0600

    Added functioning prototype of popup menu, all interactable animal characters and animations, and scripting for popup and data management

--- chore_xplorer.gd
+++ chore_xplorer.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 @onready var pause_menu: CanvasLayer = $PauseMenu
 @onready var pause_panel: Panel = $PauseMenu/PausePanel
-
+@onready var info_popup_container = $infoPopup/Overlay/HBoxContainer/InfoVboxContainer
+var popup_height = 300
 var is_paused: bool = false
 
 func _ready():
@@ -36,11 +37,14 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):  # ESC key
 		toggle_pause()
+	elif event.is_action_pressed("interact"):
+		interact()
 
 func toggle_pause():
 	is_paused = !is_paused
 	pause_menu.visible = is_paused
 	get_tree().paused = is_paused
+	pass
 
 func _on_resume_pressed():
 	toggle_pause()
@@ -52,3 +56,34 @@ func _on_main_menu_pressed():
 func _on_next_level_body_entered(body):
 	if body.is_in_group("player"):
 		get_tree().change_scene_to_file("res://reward_xplorer.tscn")
+
+func hide_info_popup():
+	clear_popup_content()
+	$infoPopup.hide()
+
+func show_info_popup(data: Dictionary):
+	for key in data.keys():
+		var label = Label.new()
+		label.text = key + ": " + str(data[key])
+		info_popup_container.add_child(label)
+	$infoPopup.show()
+
+func clear_popup_content():
+	for child in info_popup_container.get_children():
+		if str(child.name) != "Title":
+			child.queue_free()
+
+func _on_dismiss_button_button_down() -> void:
+	if $infoPopup.visible:
+		hide_info_popup()
+func interact():
+	if !$infoPopup.visible:
+		var time = Time.get_datetime_dict_from_system()
+		var time_string = "%02d:%02d:%02d" % [time["hour"], time["minute"], time["second"]]
+		var test_data = {
+			"time" : time_string,
+			"current world" : "Chore Lobby"
+		}
+		show_info_popup(test_data)
+	elif $infoPopup.visible:
+		hide_info_popup()
